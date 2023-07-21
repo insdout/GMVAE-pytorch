9c21c13ad73733b0b28df7b1acb839eb170c4699
insdout <insdxout@gmail.com>
2023-07-21 23:19:29 +0600

    pep8

--- modules.py
+++ modules.py
@@ -39,7 +39,7 @@ class Qz_xy(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU()
-            )
+        )
         self.z_mean = nn.Linear(hidden_size, latent_dim)
         self.zlogvar = nn.Linear(hidden_size, latent_dim)
 
@@ -52,7 +52,7 @@ class Qz_xy(nn.Module):
         return z
 
     def forward(self, x, y):
-        h1 = self.h1(x)    # dim: Batch, hidden_size
+        h1 = self.h1(x)
         xy = torch.cat((h1, y), dim=1)
         h2 = self.h2(xy)
         # q(z|x, y)
@@ -94,8 +94,8 @@ class EncoderFC(nn.Module):
             nn.Dropout(p=self.p),
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Dropout(p=self.p) 
-            )
+            nn.Dropout(p=self.p)
+        )
 
     def forward(self, x):
         h = self.enc_block(x)
@@ -103,7 +103,12 @@ class EncoderFC(nn.Module):
 
 
 class DecoderFC(nn.Module):
-    def __init__(self, input_size, hidden_size, latent_dim, return_probs=False):
+    def __init__(self, input_size, hidden_size,
+                 latent_dim, return_probs=False):
+        """
+        Decoder Module.
+        """
+
         super(DecoderFC, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -116,108 +121,11 @@ class DecoderFC(nn.Module):
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
             nn.Linear(hidden_size, input_size),
-           )
+        )
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, z):
         out = self.dec_block(z)
         if self.return_probs:
             out = self.sigmoid(out)
-        return out
-
-
-class EncoderLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, dropout_lstm, num_layers=1, bidirectional=True):
-        super(EncoderLSTM, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.bidirectional = bidirectional
-        self.num_directions = 2 if self.bidirectional else 1
-        self.p_lstm = dropout_lstm
-
-        self.lstm = nn.LSTM(
-            input_size=input_size,
-            hidden_size=hidden_size,
-            num_layers=num_layers,
-            dropout=self.p_lstm,
-            batch_first=True,
-            bidirectional=self.bidirectional
-        )
-
-    def forward(self, x):
-        batch_size = x.shape[0]
-        _, (h_n, _) = self.lstm(x)
-        # h_n of shape (num_layers * num_directions, batch, hidden_size)
-        """
-        hidden.shape = (num_layers*num_directions, batch, hidden_size)
-        layers can be separated using h_n.view(num_layers, num_directions, batch, hidden_size)
-        So you shouldn’t simply do hidden[-1] but first do a view() to separate the num_layers and num_directions (1 or 2). If you do
-
-        hidden = hidden.view(num_layers, 2, batch, hidden_size) # 2 for bidirectional
-        last_hidden = hidden[-1]
-        then last_hidden.shape = (2, batch, hidden_size) and you can do
-
-        last_hidden_fwd = last_hidden[0]
-        last_hidden_bwd = last_hidden[1]
-
-        TODO: check if it same as
-        # Pass the input through the LSTM
-        output, (h_n, c_n) = lstm(input_data, (h0, c0))
-        Extract the last forward and backward outputs
-        last_forward_output = output[:, -1, :hidden_size]
-        last_backward_output = output[:, 0, hidden_size:]
-
-        """
-        h_n = h_n.view(self.num_layers, self.num_directions, batch_size, self.hidden_size)
-        if self.bidirectional:
-            h = torch.cat((h_n[-1, -2, :, :], h_n[-1, -1, :, :]), dim=1)
-        else:
-            h = h_n[-1, -1, :, :]
-        return h
-
-
-class DecoderLSTM(nn.Module):
-    def __init__(self,
-                 input_size,
-                 hidden_size,
-                 latent_dim,
-                 window_size,
-                 dropout_lstm,
-                 dropout_layer,
-                 num_layers=1,
-                 bidirectional=True
-                 ):
-        super(DecoderLSTM, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.latent_dim = latent_dim
-        self.num_layers = num_layers
-        self.bidirectional = bidirectional
-        self.window_size = window_size
-        self.p_lstm = dropout_lstm
-        self.p_dropout_layer = dropout_layer
-        self.num_directions = 2 if self.bidirectional else 1
-
-        self.lstm_to_hidden = nn.LSTM(
-            input_size=latent_dim,
-            hidden_size=hidden_size,
-            num_layers=num_layers,
-            dropout=self.p_lstm,
-            batch_first=True,
-            bidirectional=self.bidirectional
-        )
-        self.dropout_layer = nn.Dropout(self.p_dropout_layer)
-
-        self.lstm_to_output = nn.LSTM(
-            input_size=self.num_directions * hidden_size,
-            hidden_size=input_size,
-            batch_first=True
-        )
-
-    def forward(self, z):
-        latent_z = z.unsqueeze(1).repeat(1, self.window_size, 1)
-        out, _ = self.lstm_to_hidden(latent_z)
-        out = self.dropout_layer(out)
-        out, _ = self.lstm_to_output(out)
         return out
